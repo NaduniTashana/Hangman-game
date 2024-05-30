5e5a0c5a21633a535792a9209f4b46af65238099
NaduniTashana <nadutash0@gmail.com>
2024-05-30 14:33:14 +0530

    first-commit

---
+++ vite-project/src/Keyboard.tsx
@@ -0,0 +1,29 @@
+
+import styles from "./Keyboard.module.css"
+
+const KEYS = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+
+
+
+export function Keyboard(){
+    return (
+    <div style={{
+        display:"grid",
+         gridTemplateColumns: "repeat(auto-fit,minmax(75px, 1fr))",
+          gap: ".5rem",}}
+    >
+       {KEYS.map(key => {
+        return <button className={`${styles.btn}  `}
+
+        key={key}>
+            {key}
+        </button>
+       })}
+
+
+        </div>
+    )
+}
